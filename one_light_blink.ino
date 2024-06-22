28339f9863666c6ce9d77ec5a0c810cd5f54b6fd
BOT-Lee <122401664+BOT-Lee@users.noreply.github.com>
2024-06-22 15:00:46 -0400

    Added Microcontroller "Hello World" Source Code
    Added both "one_light_blink.ino" and "three_light_blink.ino".

---
+++ one_light_blink.ino
@@ -0,0 +1,21 @@
+#define redLED 8
+#define pushButton 9
+
+void setup() {
+  // put your setup code here, to run once:
+  // Initialize digital pin redLED as an output.
+  pinMode(redLED, OUTPUT);
+
+  // Initialize pushButton as input.
+  pinMode(pushButton, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(pushButton) == HIGH) {
+    digitalWrite(redLED, HIGH);
+    delay(1000);
+    digitalWrite(redLED, LOW);
+    delay(1000);
+  }
+}
---
+++ three_light_blink.ino
@@ -0,0 +1,33 @@
+#define redLED 8
+#define yellowLED 6
+#define orangeLED 7
+#define pushButton 9
+
+void setup() {
+  // put your setup code here, to run once:
+  // Initialize digital pins redLED, yellowLED, and orangeLED as outputs.
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(orangeLED, OUTPUT);
+
+  // Initialize pushButton as input.
+  pinMode(pushButton, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(pushButton) == HIGH) {
+    digitalWrite(redLED, HIGH);
+    delay(1000);
+    digitalWrite(redLED, LOW);
+    delay(1000);
+    digitalWrite(yellowLED, HIGH);
+    delay(1000);
+    digitalWrite(yellowLED, LOW);
+    delay(1000);
+    digitalWrite(orangeLED, HIGH);
+    delay(1000);
+    digitalWrite(orangeLED, LOW);
+    delay(1000);
+  }
+}
